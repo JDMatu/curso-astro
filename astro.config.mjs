5ee259d863a321d1ec9653efbd7fa44aa2554bc2
JMatute <juandiegomatute11@gmail.com>
2024-03-25 18:14:06 -0700

    End Course

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://candid-caramel-4bcd4b.netlify.app/"
+  site: "https://candid-caramel-4bcd4b.netlify.app/",
+  integrations: [preact()]
 });
